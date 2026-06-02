6ffb81dfe6ba970f13fc7e10f562c242703931e6
senderultra69-cpu <senderultra69@gmail.com>
2026-06-02 18:58:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ultra_sender/',
+    base: '/sms-ultra/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
